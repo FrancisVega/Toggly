76dd38f5069b9defc7a34485fd274224d1948c9f
Francis <hisconer@gmail.com>
2016-12-30 16:16:32 +0100

    refactor: se ha eliminado la necesida de tener data-toggle-tab

--- toggly.js
+++ toggly.js
@@ -1,22 +1,23 @@
 /* global $ */
 
 (function () {
+  // Constants
   const data = {
     block: 'data-toggly',
     target: 'data-toggly-target',
-    tab: 'data-toggly-tab',
     pane: 'data-toggly-pane',
     active: 'data-toggly-active',
     id: 'data-toggly-id',
   };
 
+  // Query Toggly components
   const block = $(`[${data.block}]`);
 
-  // Por cada componente Toggly que exista.
+  // Each Toggly component in the page
   block.each(function () {
     const eachToggly = this;
     const target = $(this).attr(`${data.target}`);
-    const tabs = $(this).find(`[${data.tab}]`);
+    const tabs = $(this).find(`[${data.id}]`);
     const content = $(`[${data.pane}="${target}"]`);
     const contentTargets = $(content).find(`[${data.id}]`);
 
@@ -25,10 +26,12 @@
       $(contentTargets[idx]).attr(`${data.active}`, $(tabs[idx]).attr(`${data.active}`));
       $(this).click(function () {
         const tabId = $(this).attr(`${data.id}`);
-        // Gestionamos data-toggly-active del propio elemento toggly.
+
+        // Gestionamos data-toggly-active del propio elemento toggly
         $(eachToggly).find(`[${data.active}="true"]`).attr(`${data.active}`, 'false');
         $(this).attr(`${data.active}`, 'true');
-        // Gestionamos data-toggly-active del elemento target.
+
+        // Gestionamos data-toggly-active del elemento target
         $(content).find(`[${data.id}]`).attr(`${data.active}`, 'false');
         $(content).find(`[${data.id}='${tabId}']`).attr(`${data.active}`, 'true');
       });
