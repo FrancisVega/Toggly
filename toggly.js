f0748074996dc855542ec1cad7665dc9571498c7
Francis <hisconer@gmail.com>
2016-12-31 17:32:41 +0100

    Se han igualado los comentarios

--- toggly.js
+++ toggly.js
@@ -14,23 +14,27 @@
 
   // Each Toggly component in the page
   togglyComp.each(function () {
+    // Togglys
     const eachToggly = this;
     const target = $(this).attr(`${data.block}`);
     const tabs = $(this).find(`[${data.id}]`);
+
+    // Contents
     const content = $(`[${data.pane}="${target}"]`);
     const contentTargets = $(content).find(`[${data.id}]`);
 
+    // Each Toggly tab
     tabs.each(function (index) {
-      // Igualamos los data-toggly-active del contenedor con los del componente Toggly
+      // Igualamos los data-tg-active del contenedor con los del componente Toggly
       $(contentTargets[index]).attr(`${data.active}`, $(tabs[index]).attr(`${data.active}`));
       $(this).click(function () {
         const tabId = $(this).attr(`${data.id}`);
 
-        // Gestionamos data-toggly-active del propio elemento toggly
+        // Gestionamos data-tg-active del propio elemento toggly
         $(eachToggly).find(`[${data.active}="true"]`).attr(`${data.active}`, 'false');
         $(this).attr(`${data.active}`, 'true');
 
-        // Gestionamos data-toggly-active del elemento target
+        // Gestionamos data-tg-active del elemento target
         $(content).find(`[${data.id}]`).attr(`${data.active}`, 'false');
         $(content).find(`[${data.id}='${tabId}']`).attr(`${data.active}`, 'true');
       });
--- toggly.plain.js
+++ toggly.plain.js
@@ -24,11 +24,12 @@
 
     // Each Toggly tab
     for (let j = 0; j < tabs.length; j += 1) {
+      // Igualamos los data-tg-active del contenedor con los del componente Toggly
       contentTargets[j].setAttribute(`${data.active}`, tabs[j].getAttribute(`${data.active}`));
       tabs[j].onclick = function () {
         const tabId = this.getAttribute(`${data.id}`);
 
-        // Gestionamos data-toggly-active del propio elemento toggly
+        // Gestionamos data-tg-active del propio elemento toggly
         const togglyActive = eachToggly.querySelectorAll(`[${data.active}="true"]`);
         for (let x = 0; x < togglyActive.length; x += 1) {
           togglyActive[x].setAttribute(`${data.active}`, 'false');
@@ -36,7 +37,7 @@
 
         this.setAttribute(`${data.active}`, 'true');
 
-        // Gestionamos data-toggly-active del elemento target
+        // Gestionamos data-tg-active del elemento target
         const contentId = content.querySelectorAll(`[${data.id}]`);
         for (let x = 0; x < contentId.length; x += 1) {
           contentId[x].setAttribute(`${data.active}`, 'false');
